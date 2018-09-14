9f8639660a8c41e5ec06144a67e9008c58d72801
seek <seek.yu@100cb.cn>
2018-09-14 23:12:20 +0800

    Initial Commit

---
+++ YPWaterFlowLayout/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YPWaterFlowLayout
+//
+//  Created by 余鹏 on 2018/9/14.
+//  Copyright © 2018年 seek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
